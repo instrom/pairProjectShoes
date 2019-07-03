dd01bc4a5f1d575479e70955217c57054cd4c766
Rhamadan Yahya <Rhamdan25@gmail.com>
2019-07-03 14:20:43 +0700

    shoes

--- routers/shoesRouter.js
+++ routers/shoesRouter.js
@@ -3,12 +3,14 @@ const ShoesController = require('../Controllers/shoesController')
 const cekAdmin = require('../middlewares/cekAdmin')
 const userLogin = require('../middlewares/userLogin')
 
-router.get('/',ShoesController.readShoes)
-router.get('/add', )
+router.get('/', ShoesController.readShoes)
+router.get('/add', (req, res)=>{
+    res.render('addShoes.ejs')
+} )
 router.post('/add', userLogin, cekAdmin, ShoesController.create)
-
 router.get('/edit/:id', userLogin, cekAdmin, ShoesController.getEditPage)
 router.post('/edit/:id', userLogin, cekAdmin, ShoesController.edit)
+router.get('/delete/:id',  userLogin, cekAdmin, ShoesController.delete)
+router.get('/:brand', ShoesController.readBrand)
 
-router.get('/delete/:id', )
 module.exports = router
