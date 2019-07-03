cd5d09b0bb9a7c517e01a7757c9367866c35265b
Rhamadan Yahya <Rhamdan25@gmail.com>
2019-07-03 11:42:36 +0700

    shoes

--- Controllers/shoesController.js
+++ Controllers/shoesController.js
@@ -13,7 +13,7 @@ class ShoesController {
 
     static create(req,res){
         Model.Shoe.create(req.body)
-            .then(shoe =>{
+            .then(data =>{
                 res.redirect('/')
             })
             .catch(err=>{
@@ -21,6 +21,37 @@ class ShoesController {
             })
     }
 
+    static getEditPage(req,res){
+        Model.Shoe.findByPK(req.params.id)
+            .then(data =>{
+                res.render('shoes.ejs', {
+                    data: data
+                })
+            })
+            .catch(err=>{
+                res.send(err)
+            })
+    }
+
+    static edit(req, res) {
+        let obj = {
+            name: req.body.name,
+            brand: req.body.brand,
+            type: req.body.type,
+            price: req.body.price
+        }
+        Shoe.update(obj, {
+                where: {
+                    id: req.params.id
+                }
+            })
+            .then(updates => {
+                res.redirect('/subjects')
+            })
+            .catch(err => {
+                res.send(err)
+            })
+    }
 }
 
 module.exports = ShoesController
