cb495f8f614467589c7f4cb096f2c5247847ebb3
Tommy Sutjipto <tommysutjipto96@gmail.com>
2019-07-05 02:18:08 +0700

    views

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express()
 const port = process.env.PORT || 4000
 const session = require('express-session')
 app.set('view engine', 'ejs');
-app.use(express.static(__dirname + '/Views'));
+app.use(express.static(__dirname + '/views'));
 app.use(express.urlencoded({ extended: false }));
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 var flash = require('express-flash')
