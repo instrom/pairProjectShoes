b941d11576d7c6bf9288aa22e4306055124ec945
Tommy Sutjipto <tommysutjipto96@gmail.com>
2019-07-05 02:23:51 +0700

    sls

--- Views/main.js
+++
@@ -1,7 +0,0 @@
-$('.open-menu-icon').on('click',function() {
-    $('.main-nav-outer').addClass('open-menu');
-});
-
-$('.menu-close-icon').click(function() {
-    $('.main-nav-outer').removeClass('open-menu');
-});
